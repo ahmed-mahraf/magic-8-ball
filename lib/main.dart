4b475381116178ca43e1fb961123975e803ead5f
Mahraf Ahmed <ahmed.mahraf@outlook.com>
2021-05-12 09:43:23 +0100

    Create a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,35 @@ import 'package:flutter/material.dart';
 // Run the application
 void main() {
   runApp(
-      MaterialApp(
-        home: MainPage()
-      )
+    MaterialApp(home: MainPage()),
   );
 }
 
 //Create Stateless widget for page layout
 class MainPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text(
-          'Ask Me Anything',
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
         ),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.blue,
-      body: Container(
-      ),
+        body: EightBall());
+  }
+}
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
